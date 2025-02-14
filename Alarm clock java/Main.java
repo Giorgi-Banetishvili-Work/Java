ac32549f56023eb6124ed7b5e294de4c7c343c5e
Giorgi-Banetishvili-Work <giorgibanetishviliwork@gmail.com>
2025-02-14 14:50:42 +0400

    Add files via upload

---
+++ Alarm clock java/Main.java
@@ -0,0 +1,31 @@
+import java.time.DateTimeException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime alarmTime = null;
+        String filePath = "alarmSound.wav";
+
+        while (alarmTime == null) {
+            try {
+                System.out.print("Enter an alarm time (HH:MM:SS): ");
+                String inputTime = scanner.nextLine();
+
+                alarmTime = LocalTime.parse(inputTime, formatter);
+                System.out.println("Alarm set for " + alarmTime);
+            } catch (DateTimeException e) {
+                System.out.println("Invalid Format. Please use HH:MM:SS");
+            }
+        }
+
+        AlarmClock alarmClock = new AlarmClock(alarmTime, filePath, scanner);
+        Thread alarmThread = new Thread(alarmClock);
+        alarmThread.start();
+
+    }
+}
